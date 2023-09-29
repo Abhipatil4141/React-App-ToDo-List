e4949d15f9d129194caa616845b207c4fa8236a9
Abhi patil <abhippatil4141@gmail.com>
2023-09-29 16:12:01 +0530

    commit

---
+++ src/Componet/Todo.js
@@ -0,0 +1,137 @@
+import React, { useState,useEffect } from 'react'
+import "./Style.css"
+
+
+// how to get local storage data back 
+
+const getLocalData = ()=>{
+  const lists = localStorage.getItem('mytolist')
+
+  if (lists){
+    return JSON.parse(lists)
+  }else{
+    return[]
+  }
+}
+const Todo = () => {
+  const [inputdata ,setInputData]=useState("")
+  const [items ,setItems]=useState(getLocalData())
+  const [isEditIteam,setIsEditeItem] =useState('')
+  const [toggleBtn,setToggleBtn]=useState(false)
+
+
+  // add the items fuction 
+
+  const addItem = ()=>{
+    if(!inputdata){
+      alert('plz fill the data')
+    }else if(inputdata && toggleBtn){
+      setItems(
+        items.map((curElem)=>{
+          if (curElem.id === isEditIteam){
+            return{... curElem,name:inputdata}
+          }
+          else{
+            return curElem;
+          }
+        })
+      );
+      setInputData([])
+      setIsEditeItem(null);
+      setToggleBtn(false)
+    }
+    else{
+      const myNewInputData ={
+        id:new Date().getTime().toString(),
+        name:inputdata,
+      }
+      setItems([...items ,myNewInputData])
+      setInputData('')
+    }
+  }
+
+  // editing the iteam 
+  const editItem = (index)=>{
+    const item_todo_edited = items.find((curElem)=>{
+      return curElem.id === index
+
+    })
+    setInputData(item_todo_edited.name)
+    setIsEditeItem(index);
+    setToggleBtn(true)
+  }
+
+
+  // how to delete items 
+   const deleteItem =(index)=>{
+    const updatedItem =items.filter((curElem)=>{
+      return curElem.id !== index
+    });
+    setItems(updatedItem)
+   }
+
+
+  //  removeall element
+
+  const removeAll=()=>{
+    setItems([])
+
+  }
+
+
+  // adding useEffect
+
+  useEffect(()=> {
+    window.localStorage.setItem('mytolist', JSON.stringify(items))
+  },[items])
+  return (
+    <>
+      <div className='main-div'>
+        <div className='child-div'>
+            <figure>
+                <img src='./images/todo.svg' alt='todologo'/>
+                <figcaption>Add Your List Here</figcaption>
+            </figure>
+            <div className='addItems'>
+              <input type='text' placeholder='✍️ Add Item' className='form-control'
+              value={inputdata}
+              onChange={(e)=>setInputData(e.target.value)}/>
+
+              {toggleBtn ?(
+               <i className ="fa fa-edit add-btn" onClick={addItem}></i>
+              ):(
+                <i className ="fa fa-plus add-btn" onClick={addItem}></i>
+              )}
+            </div>
+
+            {/* show our item  */}
+
+            {items.map((curElem ,index)=>{
+              return(
+                <div className='showItems'>
+                <div className='eachItem' key={curElem.id}>
+                  <h3>{curElem.name}</h3>
+                  <div className='todo-btn'>
+                  <i className ="far fa-edit add-btn" onClick={()=>editItem(curElem.id)}></i>
+                  <i className ="far fa-trash-alt add-btn" onClick={()=>deleteItem(curElem.id)}></i>
+                  </div>
+                </div>
+              </div>
+              )
+            })}
+
+           
+
+            {/* remove all btn  */}
+            <div className='showItems'>
+              <button className='btn effect04'data-sm-link-text ='Remove All' onClick={removeAll}>
+                <span >CHECK LIST</span>
+              </button>
+            </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Todo
